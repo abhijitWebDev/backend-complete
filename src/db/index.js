4feb6fd6bb443a7a11f489828031722f83880412
abhijitWebDev <110275876+abhijitWebDev@users.noreply.github.com>
2024-02-22 18:28:56 +0530

    utility code discussion and kt

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import {DB_NAME} from '../constants.js';
+import {DB_NAME} from "../constants.js";
 
 const connectDb = async() => {
     try {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import dotenv from 'dotenv';
-import connectDb from './db/index.js';
+import dotenv from "dotenv";
+import connectDb from "./db/index.js";
 
 dotenv.config({
-    path: './.env'
+    path: "./.env"
 
 });
 
-connectDb();
+connectDb()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Server is running on port", process.env.PORT);
+    });
+})
+.catch(error => console.error(error));
 
 
 
